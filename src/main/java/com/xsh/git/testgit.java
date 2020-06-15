7639712fd12ebc9d3e9390580365d14999574fb2
徐胜豪 <7695699+xshlzw@user.noreply.gitee.com>
2020-06-16 00:19:17 +0800

    update src/main/java/com/xsh/git/testgit.java.

--- src/main/java/com/xsh/git/testgit.java
+++ src/main/java/com/xsh/git/testgit.java
@@ -3,6 +3,7 @@ package com.xsh.git;
 public class testgit {
 
     public static void main(String[] args) {
-        System.out.println("测试git1");
+        System.out.println("测试git1")；
+        System.out.println("来自gitee的添加");
     }
 }
