ed5698aa64131efc9a1123f708c59400f9b0e086
xsh_glb <xsh_glb@qq.com>
2020-06-16 00:25:31 +0800

    second update

--- src/main/java/com/xsh/git/testgit.java
+++ src/main/java/com/xsh/git/testgit.java
@@ -5,7 +5,6 @@ public class testgit {
     public static void main(String[] args) {
         System.out.println("测试git1");
         System.out.println("来自github的添加");
-        System.out.println("测试git1")；
         System.out.println("来自gitee的添加");
     }
 }
